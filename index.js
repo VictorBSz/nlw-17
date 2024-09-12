5593ce708686a5eff9dc5d520359d7ed536f5b05
Victor Souza <victor.souza0350@gmail.com>
2024-09-12 18:28:18 -0300

    feat: added task check/uncheck

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { select, input } = require('@inquirer/prompts')
+const { select, input, checkbox } = require('@inquirer/prompts')
 /* Aqui criamos a var select, e que ele deve trazer o modulo da biblioteca inquirer */
 let meta = {
     value: "Tomar 3L de agua",
@@ -7,6 +7,7 @@ let meta = {
 
 let metas = [ meta ]
 
+// FUNCTIONS
 const cadastrarMeta = async () => {
     const meta = await input ({ message: "Digite a meta:"})
     if(meta.length <= 1) {
@@ -17,8 +18,38 @@ const cadastrarMeta = async () => {
     metas.push(
         {value: meta, checked: false}
     )
+    console.log("Meta cadastrada com sucesso!")
 }
 
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: "↑↓←→: Selecionar meta | Espaço: Marcar/Desmarcar | Enter: Finalizar essa etapa",
+        choices: [...metas], // A reticencias indica para o app jogar tudo do array "metas" dentro do choices
+        instructions: false
+    })
+
+    if(respostas.length == 0) {
+        console.log("Nenhuma meta selecionada!")
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => {   // forEach para passar a function por cada resposta em respostas
+        const meta = metas.find((m) => {    // find para encontrar a meta em metas, testando uma a uma
+            return m.value == resposta
+        })  
+
+        meta.checked = true 
+
+    })
+
+    console.log("Meta(s) marcada(s) como concluída(s)")
+}
+
+// MENU E SUAS OPÇÕES
 const start = async () => {
     while(true) {
         
@@ -44,10 +75,9 @@ const start = async () => {
         switch(option) {
             case "cadastrar":
                 await cadastrarMeta()
-                console.log(metas)
                 break
             case "listar":
-                console.log('vamos listar')
+                await listarMetas()
                 break
             case "sair":
                 console.log("Até a proxima!")
